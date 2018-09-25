9e3aa4bd6fde3224069b96b8d8240d866bb0680f
William Gu <williamgu2011@hotmail.com>
2018-09-25 10:10:36 -0700

    Test commit

--- meetback/node_modules/hashids/tests-mjs/importing.mjs
+++
@@ -1,8 +0,0 @@
-import Hashids from '../';
-import chai from 'chai';
-
-const {assert} = chai;
-
-assert.isFunction(Hashids);
-const instance = new Hashids('Not Real', 5, 'ABCDEFGHJKMNPQRTWXY234689');
-assert.instanceOf(instance, Hashids);

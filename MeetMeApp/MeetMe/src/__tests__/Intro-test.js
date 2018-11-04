40199600d68e9f79f3d16356f220492beb2bb727
ARXII-13 <wzq950720@hotmail.com>
2018-11-03 17:14:27 -0700

    Add initial attempt for testing

---
+++ MeetMeApp/MeetMe/src/__tests__/Intro-test.js
@@ -0,0 +1,10 @@
+// __tests__/Intro-test.js
+import React from 'react';
+import Intro from '../Intro';
+
+import renderer from 'react-test-renderer';
+
+test('renders correctly', () => {
+  const tree = renderer.create(<Intro />).toJSON();
+  expect(tree).toMatchSnapshot();
+});

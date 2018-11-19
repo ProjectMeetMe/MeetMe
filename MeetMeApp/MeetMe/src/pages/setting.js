7349083c1c27eb9dd42e46a96711dc7892cc6930
andrewliu5050 <andrewliu5050@gmail.com>
2018-11-19 01:31:46 -0800

    displays half checkboxes half buttons

--- MeetMeApp/MeetMe/src/pages/setting.js
+++ MeetMeApp/MeetMe/src/pages/setting.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
-import { AppRegistry,View,Text,StyleSheet, ScrollView, Alert, TouchableOpacity} from "react-native";
+import { AppRegistry,View,Text,StyleSheet, ScrollView, TouchableOpacity, Alert} from "react-native";
 import NavBar from "react-native-nav";
 import NavigationForm from "../components/navigationForm";
+import update from 'immutability-helper';
+import CheckBox from 'react-native-check-box'
 
 import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
 
@@ -10,75 +12,54 @@ export default class Setting extends Component{
     super(props);
     this.state = {
       tableHead: ['', 'SUN', 'MON', 'TUE', 'WED', 'THUR', 'FRI', 'SAT'],
-      widthArr: [50, 50, 50, 50, 50, 50, 50, 50],
-      tableData: [ 
-      ['00:00', '', '', '', '', '', '', ''],
-      ['00:30', '', '', '', '', '', '', ''],
-      ['01:00', '', '', '', '', '', '', ''],
-      ['01:30', '', '', '', '', '', '', ''],
-      ['02:00', '', '', '', '', '', '', ''],
-      ['02:30', '', '', '', '', '', '', ''],
-      ['03:00', '', '', '', '', '', '', ''],
-      ['03:30', '', '', '', '', '', '', ''],
-      ['04:00', '', '', '', '', '', '', ''],
-      ['04:30', '', '', '', '', '', '', ''],
-      ['05:00', '', '', '', '', '', '', ''],
-      ['05:30', '', '', '', '', '', '', ''],
-      ['06:00', '', '', '', '', '', '', ''],
-      ['06:30', '', '', '', '', '', '', ''],
-      ['07:00', '', '', '', '', '', '', ''],
-      ['07:30', '', '', '', '', '', '', ''],
-      ['08:00', '', '', '', '', '', '', ''],
-      ['08:30', '', '', '', '', '', '', ''],
-      ['09:00', '', '', '', '', '', '', ''],
-      ['09:30', '', '', '', '', '', '', ''],
-      ['10:00', '', '', '', '', '', '', ''],
-      ['10:30', '', '', '', '', '', '', ''],
-      ['11:00', '', '', '', '', '', '', ''],
-      ['11:30', '', '', '', '', '', '', ''],
-      ['12:00', '', '', '', '', '', '', ''],
-      ['12:30', '', '', '', '', '', '', ''],
-      ['13:00', '', '', '', '', '', '', ''],
-      ['13:30', '', '', '', '', '', '', ''],
-      ['14:00', '', '', '', '', '', '', ''],
-      ['14:30', '', '', '', '', '', '', ''],
-      ['15:00', '', '', '', '', '', '', ''],
-      ['15:30', '', '', '', '', '', '', ''],
-      ['16:00', '', '', '', '', '', '', ''],
-      ['16:30', '', '', '', '', '', '', ''],
-      ['17:00', '', '', '', '', '', '', ''],
-      ['17:30', '', '', '', '', '', '', ''],
-      ['18:00', '', '', '', '', '', '', ''],
-      ['18:30', '', '', '', '', '', '', ''],
-      ['19:00', '', '', '', '', '', '', ''],
-      ['19:30', '', '', '', '', '', '', ''],
-      ['20:00', '', '', '', '', '', '', ''],
-      ['20:30', '', '', '', '', '', '', ''],
-      ['21:00', '', '', '', '', '', '', ''],
-      ['21:30', '', '', '', '', '', '', ''],
-      ['22:00', '', '', '', '', '', '', ''],
-      ['22:30', '', '', '', '', '', '', ''],
-      ['23:00', '', '', '', '', '', '', ''],
-      ['23:30', '', '', '', '', '', '', ''],
-    ]}
+      widthArr: [60, 50, 50, 50, 50, 50, 50, 50],
+      tableData: [],
+      isChecked: false
+    }
   }
 
-	_alertIndex(index) {
-    Alert.alert(`This is row ${index + 1}`);
+  selectButton(value) {
+    Alert.alert(`This value is ${value}`);
   }
 
+  unselectButton(value) {
+    Alert.alert(`This value is ${value}`);
+
+  }
+	
 	render(){
+    const state = this.state;
 
-    const element = (data, index) => (
-      <TouchableOpacity onPress={() => this._alertIndex(index)}>
-        <View style={styles.btn}>
-          <Text style={styles.btnText}> </Text>
+    const checkBox = (value) => (
+      <CheckBox
+        style={{flex: 1, padding: 10}}
+        onClick={()=>{
+          this.setState({
+            isChecked: !this.state.isChecked
+          })
+        }}
+        isChecked = {this.state.isChecked}
+      //  leftText = {"CheckBox"}
+      />
+    )
+
+    const selectButton = (value) => (
+      <TouchableOpacity onPress={() => this.selectButton(value)}>
+        <View style={styles.selectButton}>
+          <Text style={styles.btnText}>*</Text>
+        </View>
+      </TouchableOpacity>
+    )
+    
+    const unselectButton = (value) => (
+      <TouchableOpacity onPress={() => this.unselectButton(value)}>
+        <View 
+         style={styles.unselectButton}>
+          <Text style={styles.btnText}>!</Text>
         </View>
       </TouchableOpacity>
     );
 
-    const state = this.state;
-/*    state.tableData = [];
     var half_hour = false;
     for (let i = 0; i < 48; i += 1) {
       const rowData = [];
@@ -89,12 +70,12 @@ export default class Setting extends Component{
         if(time <= 9) {
           rowData.push(`${0}${time}${':'}${0}${0}`);  
           for (let fill = 0; fill < 7; fill++ ) { 
-            rowData.push(`0`); 
+            rowData.push(checkBox('0')); 
           }
         } else {
           rowData.push(`${time}${':'}${0}${0}`);
           for (let fill = 0; fill < 7; fill++ ) { 
-            rowData.push(`0`); 
+            rowData.push(unselectButton('0')); 
           }
         }
         half_hour = true;
@@ -104,18 +85,21 @@ export default class Setting extends Component{
         if(time <= 9) {
           rowData.push(`${0}${time}${':'}${3}${0}`);  
           for (let fill = 0; fill < 7; fill++ ) { 
-            rowData.push(`0`); 
+            rowData.push(checkBox('0')); 
           }
         } else {
           rowData.push(`${time}${':'}${3}${0}`);
           for (let fill = 0; fill < 7; fill++ ) { 
-            rowData.push(`0`); 
+            rowData.push(unselectButton('0')); 
           }
         }
         half_hour = false;
     }
       state.tableData.push(rowData);
-    } */
+  //    this.setState({
+    //    tableData: update(this.state.tableData, {1: {$set: '1'}})
+     // })
+    }
     
 		return(
       <View style={{flex: 1}}>
@@ -124,27 +108,19 @@ export default class Setting extends Component{
           <View>
             <Table borderStyle={{borderColor: '#C1C0B9'}}>
               <Row data={state.tableHead} widthArr={state.widthArr} style={styles.header} textStyle={styles.text2}/>
-              
             </Table>
             <ScrollView style={styles.dataWrapper}>
               <Table borderStyle={{borderColor: '#C1C0B9'}}>
-                
                 {
-                  state.tableData.map((rowData, index) => (  
-                    <TableWrapper key={index} widthArr={state.widthArr} style={[styles.row, index%2 && {backgroundColor: '#F7F6E7'}]}> 
-                    {
-                      rowData.map((cellData, cellIndex) => (
-                        <Cell key={cellIndex} data={cellIndex === 2 ? element(cellData, index) : cellData} textStyle={styles.text}/> 
-                      ))
-                    }           
-                      <Row
-               //         key={index}
-                //        data={rowData}
-                //        widthArr={state.widthArr}
-               //         style={[styles.row, index%2 && {backgroundColor: '#F7F6E7'}]}
-               //         textStyle={styles.text2}
-                      />  
-                    </TableWrapper>                   
+                  state.tableData.map((rowData, index) => (
+                    
+                    <Row
+                      key={index}
+                      data={rowData}
+                      widthArr={state.widthArr}
+                      style={[styles.row, index%2 && {backgroundColor: '#F7F6E7'}]}
+                      textStyle={styles.text2}
+                    />
                   ))
                 }
               </Table>
@@ -181,7 +157,9 @@ const styles = StyleSheet.create({
   header: { height: 50, backgroundColor: '#537791' },
   text2: { textAlign: 'center', fontWeight: '100' },
   dataWrapper: { marginTop: -1 },
-  row: {flexDirection: 'row', height: 40, backgroundColor: '#E7E6E1' },
-  btn: { width: 30, height: 18, backgroundColor: '#78B7BB',  borderRadius: 2 },
-  btnText: { textAlign: 'center', color: '#fff' }
+  row: { height: 40, backgroundColor: '#E7E6E1' },
+  selectButton: { width: 40, height: 35, marginLeft: 5, backgroundColor: '#ffffff', borderRadius: 2 },
+  unselectButton: { width: 40, height: 35, marginLeft: 5, backgroundColor: '#c8e1ff', borderRadius: 2 },
+  btnText: { textAlign: 'center' }
+  
 });
